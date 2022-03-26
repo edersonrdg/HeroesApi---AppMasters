6c3f4ec1124d31ea5cdda51c5d5c7ed307d41094
Ederson S <edersonrodrigo31@gmail.com>
2022-03-26 16:06:00 -0300

    fix: folder modules by usecase

--- src/modules/hero/controllers/getAllHeroesController.ts
+++
@@ -1,9 +0,0 @@
-import { noContent } from '../../../helpers/http';
-import { IController } from '../../../interfaces/controller';
-import { IHttpResponse } from '../../../interfaces/http-response';
-
-export class GetAllHeroesController implements IController {
-  async handle(): Promise<IHttpResponse> {
-    return noContent();
-  }
-}
