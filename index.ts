770fd27cbfdf0418965cbcff32351fb9cc13ef6c
Ederson S <edersonrodrigo31@gmail.com>
2022-03-26 15:28:47 -0300

    feat: apollo-server config

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import 'reflect-metadata'
+import { startApolloServer } from './src/services/apollo-server-express'
 import app from './src/services/express/app'
+import { buildGplSchema } from './src/services/graphql'
 
 const PORT = 8080
 
 const main = async () => {
+  const schema = await buildGplSchema()
+  const apolloserver = await startApolloServer(schema)
+
   app.listen(PORT, () => console.log(`server starting on http://localhost:${PORT}`))
 }
 
---
+++ src/services/apollo-server-express/index.ts
@@ -0,0 +1,16 @@
+import { ApolloServer } from "apollo-server-express";
+import { GraphQLSchema } from "graphql";
+
+import app from '../express/app'
+
+export const startApolloServer = async (schema: GraphQLSchema) => {
+  const server = new ApolloServer({
+    schema,
+  });
+
+  await server.start()
+
+  server.applyMiddleware({ app })
+
+  return server
+}
